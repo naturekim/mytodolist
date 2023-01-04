53a9429544cc7960038cab1247e90f7ff0514d30
jykim <jykim.developer@gmail.com>
2023-01-04 20:43:20 +0900

    todo 추가 기능 - 새로고침해도 리스트 유지되도록(localstorage사용)

--- js/todolist.js
+++ js/todolist.js
@@ -1,8 +1,14 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
+let todos = [];
 
-function addTodo(newTodo) {
+function saveTodos(newTodo) {
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function paintTodo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
     span.innerText = newTodo;
@@ -18,8 +24,19 @@ function addTodo(newTodo) {
 
 function handleTodoSubmit (event) {
     event.preventDefault();
-    addTodo(todoInput.value);
+    const newTodo = todoInput.value;
+    paintTodo(newTodo);
+    saveTodos(newTodo);
     todoInput.value = "";
+}
+
+
+// 페이지 처음 로드할 때, 로컬스토리지에 저장된 todos 불러와서 보여주기
+const savedTodos = localStorage.getItem("todos");
+if(savedTodos) {
+    const parsedTodos = JSON.parse(savedTodos);
+    todos = parsedTodos;
+    todos.forEach(paintTodo);
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
