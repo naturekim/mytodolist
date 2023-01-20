af876437a8ebb224aa852c449031e365fe4ab386
jykim <jykim.developer@gmail.com>
2023-01-20 23:14:19 +0900

    코드 정렬, 기능 - Edit 모드 상태 구분짓기 위해 Edit 버튼 색상 변경, HTML/CSS - id/class 수정, 피드백 폼  required 제거

--- js/todolist.js
+++ js/todolist.js
@@ -18,10 +18,10 @@ const feedbackFormGroup = document.getElementById("feedback-form-group");
 
 const progressGroup = document.getElementById("progress-num-bar");
 const progressText = progressGroup.querySelector("label");
-const progressBar = document.getElementById("progress-bar" );
-const checkedCount = document.getElementById("checked-count" );
-const todosCount = document.getElementById("todos-count" );
-const currentDate = document.getElementById("current-date" );
+const progressBar = document.getElementById("progress-bar");
+const checkedCount = document.getElementById("checked-count");
+const todosCount = document.getElementById("todos-count");
+const currentDate = document.getElementById("current-date");
 
 const TITLE = "title";
 const TODOS = "todos";
@@ -32,170 +32,176 @@ let todos = [];
 let editMode = false;
 
 // Title 수정 및 저장
-function activeTitleForm (event) {
-    titleFormGroup.classList.remove(HIDDEN);
-    title.classList.add(HIDDEN);
-    titleInput.value = event.target.innerText;
-    titleInput.focus();
+function activeTitleForm(event) {
+  titleFormGroup.classList.remove(HIDDEN);
+  title.classList.add(HIDDEN);
+  titleInput.value = event.target.innerText;
+  titleInput.focus();
 }
 function handleTitleSubmit(event) {
-    event.preventDefault();
-    const titleText = titleInput.value;
-    paintTitle(titleText)
-    localStorage.setItem("title", titleText);
+  event.preventDefault();
+  const titleText = titleInput.value;
+  paintTitle(titleText);
+  localStorage.setItem("title", titleText);
 }
 
 function paintTitle(titleText) {
-    title.innerText = titleText;
-    title.classList.remove(HIDDEN);
-    titleFormGroup.classList.add(HIDDEN);
+  title.innerText = titleText;
+  title.classList.remove(HIDDEN);
+  titleFormGroup.classList.add(HIDDEN);
 }
 
 // Feedback 수정 및 저장
 function activeFeedbackForm(event) {
-    feedbackFormGroup.classList.remove(HIDDEN);
-    feedback.classList.add(HIDDEN);
-    feedbackInput.value = event.target.innerText;
-    feedbackInput.focus();
+  feedbackFormGroup.classList.remove(HIDDEN);
+  feedback.classList.add(HIDDEN);
+  feedbackInput.value = event.target.innerText;
+  feedbackInput.focus();
 }
 
 function handleFeedbackSubmit(event) {
-    event.preventDefault();
-    const feedbackText = feedbackInput.value;
-    paintFeedback(feedbackText);
-    localStorage.setItem(FEEDBACK, feedbackText);
+  event.preventDefault();
+  const feedbackText = feedbackInput.value;
+  paintFeedback(feedbackText);
+  localStorage.setItem(FEEDBACK, feedbackText);
 }
 
 function paintFeedback(feedbackText) {
-    feedback.innerText = feedbackText;
-    feedback.classList.remove(HIDDEN);
-    feedbackFormGroup.classList.add(HIDDEN);
+  feedback.innerText = feedbackText;
+  feedback.classList.remove(HIDDEN);
+  feedbackFormGroup.classList.add(HIDDEN);
 }
 
 // Progress Bar
 function paintProgressBar() {
-    const checkedTodos = document.querySelectorAll("input[type=checkbox]:checked")
-    const checkedTodosLength = checkedTodos.length;
-    const todosLength = todos.length;
+  const checkedTodos = document.querySelectorAll(
+    "input[type=checkbox]:checked"
+  );
+  const checkedTodosLength = checkedTodos.length;
+  const todosLength = todos.length;
 
-    // Todo를 추가했다가 모두 삭제하는 경우 예외처리
-    if (todosLength === 0 || checkedTodosLength === 0) {
-        progressBar.value = 0;
-        progressText.innerText = `0%`;
-    } else {
-        checkedCount.innerText = checkedTodosLength;
-        todosCount.innerText = todosLength;
-        const ratio = Math.round(checkedTodosLength / todosLength * 100);
-        progressBar.value = ratio;
-        progressText.innerText = `${ratio}%`;
-    }
+  // Todo를 추가했다가 모두 삭제하는 경우 예외처리
+  if (todosLength === 0 || checkedTodosLength === 0) {
+    progressBar.value = 0;
+    progressText.innerText = `0%`;
+  } else {
+    checkedCount.innerText = checkedTodosLength;
+    todosCount.innerText = todosLength;
+    const ratio = Math.round((checkedTodosLength / todosLength) * 100);
+    progressBar.value = ratio;
+    progressText.innerText = `${ratio}%`;
+  }
 }
 
 // Edit Button - Todo Form
 function activeTodoForm(event) {
-    event.preventDefault();
-    if(editMode) {
-        editMode = false;
-        todoFormGroup.classList.remove(HIDDEN);
-        Array.from(deleteBtns).forEach((btn) => {
-            btn.classList.remove(HIDDEN);
-        });
-        todoInput.focus();
-    } else {
-        editMode = true;
-        todoFormGroup.classList.add(HIDDEN);
-        Array.from(deleteBtns).forEach((btn) => {
-            btn.classList.add(HIDDEN);
-        });
-    }
+  event.preventDefault();
+  if (editMode) {
+    editMode = false;
+    todoFormGroup.classList.remove(HIDDEN);
+    editBtn.classList.remove("btn-primary");
+    editBtn.classList.add("btn-sucess");
+    Array.from(deleteBtns).forEach((btn) => {
+      btn.classList.remove(HIDDEN);
+    });
+    todoInput.focus();
+  } else {
+    editMode = true;
+    todoFormGroup.classList.add(HIDDEN);
+    editBtn.classList.add("btn-primary");
+    editBtn.classList.remove("btn-sucess");
+    Array.from(deleteBtns).forEach((btn) => {
+      btn.classList.add(HIDDEN);
+    });
+  }
 }
 
 // Checkbox
-function handleChkbox (event) {
-    const li = event.target.parentElement;
-    const checkbox = event.target;
-    todos.forEach((todo) => {
-        if (todo.id === parseInt(li.id)) {
-            todo.checked = checkbox.checked;
-        }
-    });
-    saveTodos();
-    if(checkbox.checked === true) {
-        li.classList.add("disabled-text");
-    } else {
-        li.classList.remove("disabled-text");
+function handleChkbox(event) {
+  const li = event.target.parentElement;
+  const checkbox = event.target;
+  todos.forEach((todo) => {
+    if (todo.id === parseInt(li.id)) {
+      todo.checked = checkbox.checked;
     }
-    paintProgressBar();
+  });
+  saveTodos();
+  if (checkbox.checked === true) {
+    li.classList.add("disabled-text");
+  } else {
+    li.classList.remove("disabled-text");
+  }
+  paintProgressBar();
 }
 
 // Delete Button
 function deleteTodo(event) {
-    const li = event.target.parentElement;
-    li.remove();
-    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
-    saveTodos();
-    paintProgressBar();
+  const li = event.target.parentElement;
+  li.remove();
+  todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+  saveTodos();
+  paintProgressBar();
 }
 
 // Todo List
 function saveTodos() {
-    localStorage.setItem(TODOS, JSON.stringify(todos));
+  localStorage.setItem(TODOS, JSON.stringify(todos));
 }
 
 function paintTodo(newTodoObj) {
-    const li = document.createElement("li");
-    li.id = newTodoObj.id;
+  const li = document.createElement("li");
+  li.id = newTodoObj.id;
 
-    const input = document.createElement("input");
-    const cboxId = "chk_" + newTodoObj.id;
-    input.setAttribute("id", cboxId);
-    input.setAttribute("type", "checkbox");
-    if(newTodoObj.checked) {
-        input.setAttribute("checked", newTodoObj.checked);
-        li.classList.add("disabled-text");
-    }
-    input.addEventListener("click", handleChkbox);
+  const input = document.createElement("input");
+  const cboxId = "chk_" + newTodoObj.id;
+  input.setAttribute("id", cboxId);
+  input.setAttribute("type", "checkbox");
+  if (newTodoObj.checked) {
+    input.setAttribute("checked", newTodoObj.checked);
+    li.classList.add("disabled-text");
+  }
+  input.addEventListener("click", handleChkbox);
 
-    const label = document.createElement("label");
-    label.setAttribute("for", cboxId);
-    label.innerText = newTodoObj.todo;
+  const label = document.createElement("label");
+  label.setAttribute("for", cboxId);
+  label.innerText = newTodoObj.todo;
 
-    const button = document.createElement("button");
-    if(editMode) {
-        button.setAttribute("class", "btn-delete");
-    } else {
-        button.setAttribute("class", "btn-delete hidden");
-    }
-    button.addEventListener("click", deleteTodo);
+  const button = document.createElement("button");
+  if (editMode) {
+    button.setAttribute("class", "btn-delete");
+  } else {
+    button.setAttribute("class", "btn-delete hidden");
+  }
+  button.addEventListener("click", deleteTodo);
 
-    li.appendChild(input);
-    li.appendChild(label);
-    li.appendChild(button);
-    todoList.appendChild(li);
+  li.appendChild(input);
+  li.appendChild(label);
+  li.appendChild(button);
+  todoList.appendChild(li);
 }
 
 // Add Button
-function handleTodoSubmit (event) {
-    event.preventDefault();
-    const newTodoObj = {
-        id : Date.now(),
-        todo : todoInput.value,
-        checked : false
-    }
-    editMode = true; //edit모드 유지
-    paintTodo(newTodoObj);
-    todos.push(newTodoObj);
-    saveTodos();
-    paintProgressBar();
-    todoInput.value = "";
-    todoInput.focus();
+function handleTodoSubmit(event) {
+  event.preventDefault();
+  const newTodoObj = {
+    id: Date.now(),
+    todo: todoInput.value,
+    checked: false,
+  };
+  editMode = true; //edit모드 유지
+  paintTodo(newTodoObj);
+  todos.push(newTodoObj);
+  saveTodos();
+  paintProgressBar();
+  todoInput.value = "";
+  todoInput.focus();
 }
 
 // 페이지 로드 시 실행
 // Jan 19, 2023 이 형식으로 오늘 날짜 표시
 const date = new Date();
 const options = {
-//   weekday: "long",
+  //   weekday: "long",
   year: "numeric",
   day: "numeric",
   month: "short",
@@ -203,21 +209,21 @@ const options = {
 currentDate.innerText = date.toLocaleDateString("en-us", options);
 
 const savedTitle = localStorage.getItem(TITLE);
-if(savedTitle) {
-    paintTitle(savedTitle);
+if (savedTitle) {
+  paintTitle(savedTitle);
 }
 
 const savedTodos = localStorage.getItem(TODOS);
-if(savedTodos) {
-    const parsedTodos = JSON.parse(savedTodos);
-    todos = parsedTodos;
-    todos.forEach(paintTodo);
-    paintProgressBar();
+if (savedTodos) {
+  const parsedTodos = JSON.parse(savedTodos);
+  todos = parsedTodos;
+  todos.forEach(paintTodo);
+  paintProgressBar();
 }
 
 const savedFeedback = localStorage.getItem(FEEDBACK);
-if(savedFeedback) {
-    paintFeedback(savedFeedback);
+if (savedFeedback) {
+  paintFeedback(savedFeedback);
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
